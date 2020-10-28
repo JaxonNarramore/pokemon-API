05a6f5fa63bf1b9f27c32522e3590d655ab687f9
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-10-28 10:36:28 -0500

    [github.js] add name with api'

--- github.js
+++ github.js
@@ -1,9 +1,20 @@
+const container = document.querySelector('.container')
+
 fetch('https://api.github.com/users/jaxonnarramore')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData);
+    const jaxonObj = {
+        bio: githubData.bio,
+        username: githubData.login,
+        name: githubData.name
+    }
+    console.log(jaxonObj);
+    // console.log(githubData);
+    const newElement = document.createElement('p');
+    newElement.textContent = jaxonObj.name;
+    container.appendChild(newElement);
 })
 .catch(error => {
     console.log(error);
